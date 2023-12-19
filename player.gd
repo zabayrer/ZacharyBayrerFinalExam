181e20832add01b722f30443589537033f919b87
zabayrer <zabayrer@s.sfusd.edu>
2023-12-19 11:20:25 -0800

    Put player and enemy into world scene
    debugged both player and enemy

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var speed = 300
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#create velocity, set it to zero
@@ -11,12 +12,14 @@ func _process(delta):
 	if Input.is_action_pressed("move_up"):
 		velocity.y += -1
 	if Input.is_action_pressed("move_left"):
-		velocity.y += -1
+		velocity.x += -1
 	if Input.is_action_pressed("move_right"):
-		velocity.y += 1
+		velocity.x += 1
 	
 	#normalize the velocity
 	velocity = velocity.normalized()
+	print(velocity.length())
+	
 	
 	#move player
-	position += velocity * delta
+	position += velocity * speed * delta
